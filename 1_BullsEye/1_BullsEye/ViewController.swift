7ab3ca77f1fcff5195db7882c1ec5ef0070116ad
Chun Wu <the.chun.wu@gmail.com>
2020-05-26 23:08:27 -0400

    Initial Project, Basic Controls, Outlets

---
+++ 1_BullsEye/1_BullsEye/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  1_BullsEye
+//
+//  Created by Chun Wu on 2020-05-26.
+//  Copyright © 2020 Chun Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
+        startNewRound()
+    }
+    
+    @IBAction func showAlert() {
+        
+        let message = "The value of the slider is now: \(currentValue)" + "\nThe target value is: \(targetValue)."
+        let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+        startNewRound()
+    }
+
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
+    }
+    
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+    }
+}
+
