8b1943cb0711b9a1f769786703ed1426201fd87e
Chun Wu <the.chun.wu@gmail.com>
2020-05-31 20:12:12 -0400

    Initial Commit of Color Picker

---
+++ Week01/2_ColorPicker/2_ColorPicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2_ColorPicker
+//
+//  Created by Chun Wu on 2020-05-31.
+//  Copyright © 2020 Chun Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
